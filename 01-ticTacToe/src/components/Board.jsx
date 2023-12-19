890544b8603bcd042e126edd264a9ff672888e71
Adrián Téllez <adriantellezblasco@gmail.com>
2023-12-19 14:19:17 +0100

    cambios infimos

--- 01-ticTacToe/src/components/Board.jsx
+++ 01-ticTacToe/src/components/Board.jsx
@@ -1,28 +1,27 @@
 import Square from "./Square";
 
-function onHandleClick(){
-    
+function handleClick(){
+    alert("Me hiciste click");
 }
 
-//onHandleClick=() => {return "x"}
 
 export default function Board(){
     return(
         <div className="btn-group-vertical container-fluid " role="group" aria-label="Vertical button group">
             <div className="btn-group" role="group" aria-label="Basic example">
-                <Square  onClick={onHandleClick}></Square>
-                <Square onClick={onHandleClick}></Square>
-                <Square onClick={onHandleClick}></Square>
+                <Square onClick={handleClick}></Square>
+                <Square onClick={handleClick}></Square>
+                <Square onClick={handleClick}></Square>
             </div>
             <div className="btn-group" role="group" aria-label="Basic example">
-                <Square onClick={onHandleClick}></Square>
-                <Square onClick={onHandleClick}></Square>
-                <Square onClick={onHandleClick}></Square>
+                <Square onClick={handleClick}></Square>
+                <Square onClick={handleClick}></Square>
+                <Square onClick={handleClick}></Square>
             </div>
             <div className="btn-group" role="group" aria-label="Basic example">
-                <Square onClick={onHandleClick}></Square>
-                <Square onClick={onHandleClick}></Square>
-                <Square onClick={onHandleClick}></Square>
+                <Square onClick={handleClick}></Square>
+                <Square onClick={handleClick}></Square>
+                <Square onClick={handleClick}></Square>
             </div>
         </div>
     )
